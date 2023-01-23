14f79b7156d7051a781d1177b5ee971b4f2c2a47
rodrigomoraisr <53228228+rodrigomoraisr@users.noreply.github.com>
2023-01-23 16:15:44 -0300

    Tratamento de erros

--- HelloWorldAgain/HelloWorldAgain/Program.cs
+++ HelloWorldAgain/HelloWorldAgain/Program.cs
@@ -112,6 +112,53 @@ namespace HelloWorldAgain
                     continue;
                 Console.WriteLine("I will not be printed if i = 2.\n");
             }
+
+            //Tratamento de erros
+            int numerator, denominator;
+            Console.Write("Please enter the numerator: ");
+            numerator = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter the denominator: ");
+            denominator = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine("The result is {0}.", numerator / denominator);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("---- End of Error Handling Example ----");
+            }
+
+            int choice = 0;
+            int[] numbers = { 10, 11, 12, 13, 14, 15 };
+            Console.Write("Please enter the index of the array: ");
+
+            try
+            {
+                choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("numbers[{0}] = {1}", choice, numbers[choice]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Error: Index should be from 0 to 5.");
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Error: You did not enter an integer.");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+
+            }
         }
     }
 }
