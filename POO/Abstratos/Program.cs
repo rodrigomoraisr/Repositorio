7391d59b635328a6e688036648c4ce3f4517ac45
rodrigomoraisr <53228228+rodrigomoraisr@users.noreply.github.com>
2023-02-03 16:33:07 -0300

    Abstratos

---
+++ POO/Abstratos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Abstratos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassA a = new ClassA();
+            a.PrintMessage();
+            a.PrintMessageAbstract();
+            Console.Read();
+        }
+    }
+
+    abstract class MyAbstractClass
+    {
+        private string message = "Hello C#";
+        public void PrintMessage()
+        {
+            Console.WriteLine(message);
+        }
+        public abstract void PrintMessageAbstract();
+    }
+
+    class ClassA : MyAbstractClass
+    {
+        public override void PrintMessageAbstract()
+        {
+            Console.WriteLine("C# is fun!!!");
+        }
+    }
+}
