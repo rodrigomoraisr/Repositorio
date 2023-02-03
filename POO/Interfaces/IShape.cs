53bc79c36fd2d2c8501554889307bca2713b8aca
rodrigomoraisr <53228228+rodrigomoraisr@users.noreply.github.com>
2023-02-03 17:04:40 -0300

    Interfaces

---
+++ POO/Interfaces/IShape.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{   
+    interface IShape
+    {
+        int MyNumber
+        { get; set; }
+
+        void InterfaceMethod();       
+    }
+
+    class ClassA : IShape
+    {
+        private int myNumber;
+        public int MyNumber
+        {
+            get { return myNumber; }
+            set
+            {
+                if (value < 0)
+                    myNumber = 0;
+                else
+                    myNumber = value;
+            }
+        }
+
+        public void InterfaceMethod()
+        {
+            Console.WriteLine("The number is {0}.", MyNumber);
+        }
+    }
+}
---
+++ POO/Interfaces/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassA a = new ClassA();
+            a.MyNumber = 5;
+            a.InterfaceMethod();
+            Console.Read();
+        }
+    }
+}
